e7858429c04faf8c22290f2260122303b22d6292
MBashov <galacticos.mb@gmail.com>
2025-02-15 10:13:03 +0100

    Install mongoose
    Setup db connection

--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ import routes from '../routes.js';
 
 const app = express();
 
+//* Db setup
+try {
+    await mongoose.connect('mongodb://localhost:27017/GamingTeam');
+    console.log('Db conected succesfully');
+} catch (err) {
+    console.log('Cannot conect to Db');
+    console.error(err.message);
+}
+
 //* Handlebars setup
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
