734415aaf38c4fb09d7fd8d0661cee6d2b725aef
MBashov <galacticos.mb@gmail.com>
2025-02-15 10:35:59 +0100

    Add register page

--- routes.js
+++ routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import homeController from "./src/controllers/homeController.js";
+import authController from "./src/controllers/authController.js";
 
 const routes = Router();
 
 routes.use(homeController);
+routes.use('/auth', authController);
 
 export default routes;
