85fe73cf7db1429670ab93708f1f021dde5075d3
MBashov <galacticos.mb@gmail.com>
2025-02-27 11:54:30 +0100

    Add game model

--- src/models/User.js
+++ src/models/User.js
@@ -4,16 +4,19 @@ import bcrypt from 'bcrypt';
 const userSchema = new Schema ({
     username: {
         type: String,
-        required: true,
+        required: [true, 'Username is required!'],
+        minLength: [5, 'Username should be at least 5 characters long'],
     },
     email: {
         type: String,
-        required: true,
+        required: [true, 'Email is required!'],
+        minLength: [10, 'Email should be at least 10 characters long'],
     },
     password: {
         type: String,
-        required: true,
-    },
+        required: [true, 'Password is required!'],
+        minLength: [4, 'Password should be at least 4 characters long'],
+    }
 });
 
 userSchema.pre('save', async function (){
